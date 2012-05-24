d3bdd5fc4b3c657f2187e908e6ef1c878b0aa483
Brett Memsic <memsic@gmail.com>
2012-05-24 11:44:08 -0700

    Completed the layout structure

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
-
       it "should be successful" do
         get 'home'
         response.should be_success
@@ -13,12 +16,11 @@ describe PagesController do
       it "should have the right title" do
         visit '/pages/home'
         page.should have_selector("title", 
-                      :text => "Ruby on Rails Tutorial Sample App | Home")
+                                  :text => "#{@base_title} | Home")
       end
   end
 
   describe "GET 'contact'" do
-
       it "should be successful" do
         get 'contact'
         response.should be_success
@@ -27,12 +29,11 @@ describe PagesController do
       it "should have the right title" do
         visit '/pages/contact'
         page.should have_selector("title", 
-                      :text => "Ruby on Rails Tutorial Sample App | Contact")        
+                                  :text => "#{@base_title} | Contact")        
       end
   end
 
   describe "GET 'about'" do
-
       it "should be successful" do
         get 'about'
         response.should be_success
@@ -41,7 +42,7 @@ describe PagesController do
       it "should have the right title" do
         visit '/pages/about'
         page.should have_selector("title", 
-                        :text => "Ruby on Rails Tutorial Sample App | About")        
+                                  :text => "#{@base_title} | About")        
       end
   end
 end
