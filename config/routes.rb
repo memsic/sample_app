5b0e654447da53f6e2d955b651cc8c90bddd16a2
Brett Memsic <memsic@gmail.com>
2012-05-31 17:43:37 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'pages#home'
   
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/help',    to: 'pages#help'
   match '/about',   to: 'pages#about'
