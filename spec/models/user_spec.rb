01e907ac39dbc33f1e1001317a0e667b1001c6b2
Brett Memsic <memsic@gmail.com>
2012-06-01 13:11:08 -0700

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,10 +25,18 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) } 
   
   it { should be_valid }
+  it { should_not be_admin }
+  
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
   
   describe "when name is not present" do
     before { @user.name = " " }
