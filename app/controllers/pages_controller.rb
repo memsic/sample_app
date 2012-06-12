08af97becb1eb15ce95b5914f4f20c4e18aa0d33
Brett Memsic <memsic@gmail.com>
2012-06-11 17:10:49 -0700

    Add user following

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
 
   def home
     if signed_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+        @micropost  = current_user.microposts.build
+        @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
   
